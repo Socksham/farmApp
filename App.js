b2cf94bc47728db7d262c2249d6fa8bff0754c36
Socksham <60550018+Socksham@users.noreply.github.com>
2021-01-16 15:42:02 -0600

    signup login with firebase

--- App.js
+++ App.js
@@ -1,43 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Dashboard from './screens/Dashboard';
-import Homepage from './screens/Homepage';
-import * as firebase from 'firebase';
-import apiKeys from './config/keys';
+import React from 'react'
+import Login from './screens/Login'
+import SwitchNavigator from './navigation/SwitchNavigator'
 
 export default class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isLoaded: true,
-      isAuthenticationReady: false,
-      isAuthenticated: false
+    render() {
+        return <SwitchNavigator />
     }
-    //load firebase
-    if(!firebase.default.apps.length){
-      firebase.default.initializeApp(apiKeys.firebaseConfig)
-      firebase.default.auth().onAuthStateChanged((user) => {
-        this.setState({isAuthenticationReady: true})
-        this.setState({isAuthenticated:!!user})
-      })
-    }
-  }
-  render(){
-    return (
-      <View style={styles.container}>
-        {(this.state.isAuthenticated) ? <Dashboard /> : <Homepage />}
-      </View>
-    );
-  }
-
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Dashboard from './screens/Dashboard';
+// import Homepage from './screens/Homepage';
+// import * as firebase from 'firebase';
+// import apiKeys from './config/keys';
+
+// export default class App extends React.Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       isLoaded: true,
+//       isAuthenticationReady: false,
+//       isAuthenticated: false
+//     }
+//     //load firebase
+//     if(!firebase.default.apps.length){
+//       firebase.default.initializeApp(apiKeys.firebaseConfig)
+//       firebase.default.auth().onAuthStateChanged((user) => {
+//         this.setState({isAuthenticationReady: true})
+//         this.setState({isAuthenticated:!!user})
+//       })
+//     }
+//   }
+//   render(){
+//     return (
+//       <View style={styles.container}>
+//         {(this.state.isAuthenticated) ? <Dashboard /> : <Homepage />}
+//       </View>
+//     );
+//   }
+
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ config/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBK6Yoich2RAogseRjEx764cYypYkW2y5E",
+    authDomain:"farmapp-3b786.firebaseapp.com",
+    projectId: "farmapp-3b786",
+    storageBucket: "farmapp-3b786.appspot.com",
+    messagingSenderId: "114425243222",
+    appId: "114425243222:web:a7e9b975ec32089480fdf7",
+    measurementId: "G-WXYG1K0RPY",
+}
+
+const Firebase = firebase.initializeApp(firebaseConfig)
+
+export default Firebase
