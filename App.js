ce4863fd9a3a60af53024183afe4f22277e3c68a
Socksham <60550018+Socksham@users.noreply.github.com>
2021-01-16 12:08:29 -0600

    start

--- App.js
+++ App.js
@@ -2,13 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isLoaded: true,
+      isAuthenticationReady: false,
+      isAuthenticated: false
+    }
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        {(this.state.isAuthenticated) ? <Text>Logged in</Text> : <Text>Signup Page</Text>}
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
